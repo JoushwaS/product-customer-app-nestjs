e2d83fe7f076bb06abebda4206df05bd422d97cd
JoushwaS <joushwashahzad1@gmail.com>
2024-09-26 01:29:42 +0500

    linted codebase
    linted codebase

--- src/product/dto/update-product.dto.ts
+++ src/product/dto/update-product.dto.ts
@@ -1,4 +1,4 @@
-import { IsOptional, IsString } from 'class-validator';
+import { IsOptional } from 'class-validator';
 import { CreateProductDto } from './create-product.dto';
 
 export class UpdateProductDto extends CreateProductDto {
